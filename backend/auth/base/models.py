ab521ebcb10da5be8ca27a27307e5955a8dc9ad0
I-Dat Trong <anhtheboy011@gmail.com>
2023-04-21 16:42:45 +0700

    update auth

---
+++ backend/auth/base/models.py
@@ -0,0 +1,10 @@
+from bson import ObjectId
+from pydantic import BaseConfig
+from pydantic import BaseModel as BaseModelConfig
+
+class BaseModel(BaseModelConfig): 
+    class Config(BaseConfig):
+        json_encoders = {ObjectId: str}
+        allow_population_by_alias = True
+        allow_populate_by_field_name = True
+        arbitrary_types_allowed = True
