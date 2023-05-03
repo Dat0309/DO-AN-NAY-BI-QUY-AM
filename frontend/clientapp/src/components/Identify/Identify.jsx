c030fa4bcdbcf530902e33330f7cdedf1c7efe44
Duy Danh <duydanh16042019@gmail.com>
2023-05-03 17:22:30 +0700

    update

--- frontend/clientapp/src/components/Identify/Identify.jsx
+++ frontend/clientapp/src/components/Identify/Identify.jsx
@@ -1,22 +1,29 @@
 import React, { useState } from "react";
 import t from '../../translation.json';
-import srcURL from '../../srcURL.json'
+import srcURL from '../../srcURL.json';
+import { Icon } from 'semantic-ui-react'
 
+import 'semantic-ui-css/semantic.min.css'
 import './identifyStyles.scss'
+
+
 
 const Identify = () => {
 
     const [images, setImages] = useState([]);
+    const [statusButton, setStatusButton] = useState('active')
+    const [showRemoveButton, setShowRemoveButton] = useState([]);
 
     const reader = new FileReader();
 
-    let selectedImage = 'anh dang chon'
-    console.log(selectedImage);
+    let selectedImage = "";
 
+
+    // Handle upload image
     const handleImageUpload = (e) => {
+        e.preventDefault()
         let file = e.target.files[0];
         reader.readAsDataURL(file);
-        console.log('vo roi ne');
         reader.onload = () => {
             selectedImage = reader.result
             setImages(prevImages => {
@@ -25,11 +32,33 @@ const Identify = () => {
                     alert('The same image cannot be added twice');
                 } else {
                     result.push(selectedImage);
+                    setShowRemoveButton(prevStatusButton => {
+                        let statusResult = [...prevStatusButton]
+                        statusResult.push(`show${result.length}`)
+                        return statusResult;
+                    });
+                }
+                if (result.length === 4) {
+                    setStatusButton('deactive')
                 }
                 return result;
             });
         }
         e.target.value = null;
+    }
+
+    const handleRemoveImage = (e, index) => {
+        e.preventDefault()
+        setImages(prevImages => {
+            let result = [...prevImages]
+            result.splice(index, 1)
+            setShowRemoveButton(prevStatusButton => {
+                let statusResult = [...prevStatusButton]
+                statusResult.splice(index, 1);
+                return statusResult;
+            });
+            return result;
+        });
     }
 
     return (
@@ -45,7 +74,7 @@ const Identify = () => {
                 <div className="identify__box__addimage">
                     <div className="identify__box__addimage--center">
                         <input type="file" accept="image/*" onInput={(e) => handleImageUpload(e)} className="identify__box__addimage__input" id="__ADD__IMAGE" />
-                        <label className="identify__box__addimage__label" htmlFor="__ADD__IMAGE">
+                        <label className={`identify__box__addimage__label ${statusButton || ''}`} htmlFor="__ADD__IMAGE">
                             <span>{t["identify.label.addimage"]}</span>
                         </label>
                     </div>
@@ -53,17 +82,29 @@ const Identify = () => {
                 <div className="identify__box__showimage">
                     <div className="identify__box__showimage__card">
                         {images[0] && <img src={images[0]} alt="selectedImage" />}
+                        <button onClick={(e) => handleRemoveImage(e, 0)} className={`identify__box__showimage__card__removebutton ${showRemoveButton[0] || "noshow"}`}>
+                            <Icon name="x" />
+                        </button>
                     </div>
                     <div className="identify__box__showimage__card">
                         {images[1] && <img src={images[1]} alt="selectedImage" />}
+                        <button onClick={(e) => handleRemoveImage(e, 1)} className={`identify__box__showimage__card__removebutton ${showRemoveButton[1] || "noshow"}`}>
+                            <Icon name="x" />
+                        </button>
                     </div>
                 </div>
                 <div className="identify__box__showimage">
                     <div className="identify__box__showimage__card">
                         {images[2] && <img src={images[2]} alt="selectedImage" />}
+                        <button onClick={(e) => handleRemoveImage(e, 2)} className={`identify__box__showimage__card__removebutton ${showRemoveButton[2] || "noshow"}`}>
+                            <Icon name="x" />
+                        </button>
                     </div>
                     <div className="identify__box__showimage__card">
                         {images[3] && <img src={images[3]} alt="selectedImage" />}
+                        <button onClick={(e) => handleRemoveImage(e, 3)} className={`identify__box__showimage__card__removebutton ${showRemoveButton[3] || "noshow"}`}>
+                            <Icon name="x" />
+                        </button>
                     </div>
                 </div>
                 <div className="identify__box__button">
---
+++ frontend/clientapp/src/components/Result/Result.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import './resultStyles.scss'
+
+const Result = () => {
+    return (
+        <div className="result">
+             <h2 className="result__title">Result</h2>
+        </div>
+    )
+}
+
+export default Result;
